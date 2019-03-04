7d5c079e02a9c6e665ee61477a2c0d60f0a03ff3
Saint-Jean Germila <germila@noukod.com>
2019-03-04 15:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-const scuberGreetingForFeet = 0;
-let isFeet;
-if (scuberGreetingForFeet <= 199){
-  isFeet = 'This one is on me!';
-}
+// const scuberGreetingForFeet = 0;
+// let isFeet;
+// if (scuberGreetingForFeet <= 199){
+//   isFeet = 'This one is on me!';
+// }
