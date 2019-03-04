8a8c7e335b58c6ad9f4bf20b02a33fad7495076d
Saint-Jean Germila <germila@noukod.com>
2019-03-04 15:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 // Write your code in this file!
-// const scuberGreetingForFeet = 0;
-// let isFeet;
-// if (scuberGreetingForFeet <= 199){
-//   isFeet = 'This one is on me!';
-// }
-const switchOnCharmFromTip = 0;
-let isTipGenerous;
-
-switch (switchOnCharmFromTip = "Thank you so much") {
-  isTipGenerous - true;
-  case expression:
-
-    break;
-  default:
-
+const scuberGreetingForFeet = 0;
+let isFeet;
+if (scuberGreetingForFeet <= 199){
+  isFeet = 'This one is on me!';
 }
