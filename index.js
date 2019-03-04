8a72384acb04095a07a098b7de24c6606f54cdfc
Saint-Jean Germila <germila@noukod.com>
2019-03-04 15:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 // if (scuberGreetingForFeet <= 199){
 //   isFeet = 'This one is on me!';
 // }
+const switchOnCharmFromTip = 0;
+let isTipGenerous;
+
+switch (switchOnCharmFromTip = "Thank you so much") {
+  isTipGenerous - true;
+  case expression:
+    
+    break;
+  default:
+    
+}
