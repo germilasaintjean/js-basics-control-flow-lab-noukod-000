97d2d80af81727557368388c6490cd330a1762e8
Saint-Jean Germila <germila@noukod.com>
2019-03-04 15:41:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Write your code in this file!
-const scuberGreetingForFeet = 0;
-let isFeet;
-if (scuberGreetingForFeet <= 199){
-  isFeet = 'This one is on me!';
+function scuberGreetingForFeet (feet){
+  if (feet <= 400){
+    return "This is in me!";
+  } else if (1999 < feet &7 feet < 2500) {
+    return "I will glady take your thirty bucks."
+    
+  }
 }
